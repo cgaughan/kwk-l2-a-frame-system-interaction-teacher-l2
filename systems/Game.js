64d17db3c4ffe8ada9ff338cb44780a6823fb730
Caroline Gaughan <cgaughan1122@gmail.com>
2018-06-26 16:17:52 +0000

    automatically backed up by learn

--- systems/Game.js
+++ systems/Game.js
@@ -74,6 +74,7 @@ AFRAME.registerSystem('game', {
   newBallShouldSpawn: function() {
     // TODO: implement me!
     // hint: Is there some way to use Date.now() to compare the current time and the time since the last ball spawned?
+    if ()
     return false
   },
 
